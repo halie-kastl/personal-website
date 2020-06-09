ab546202e049a68f4601bc7f3e1b95651160e1cd
halie-kastl <haliekastl@gmail.com>
2020-06-09 09:16:18 -0700

    wip scroll animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,56 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title = "web";
+
+  constructor(htmlElement: HTMLElement) {}
+
+  smoothScroll(elementId) {
+    var MIN_PIXELS_PER_STEP = 16;
+    var MAX_SCROLL_STEPS = 30;
+    var target = document.getElementById(elementId);
+    var scrollContainer = target;
+    do {
+      scrollContainer = scrollContainer.parentNode as HTMLElement;
+      if (!scrollContainer) return;
+      scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop === 0);
+
+    var targetY = 0;
+    do {
+      if (target === scrollContainer) break;
+      targetY += target.offsetTop;
+    } while ((target = target.offsetParent as HTMLElement));
+
+    var pixelsPerStep = Math.max(
+      MIN_PIXELS_PER_STEP,
+      Math.abs(targetY - scrollContainer.scrollTop) / MAX_SCROLL_STEPS
+    );
+
+    var isUp = targetY < scrollContainer.scrollTop;
+
+    var stepFunc = function () {
+      if (isUp) {
+        scrollContainer.scrollTop = Math.max(
+          targetY,
+          scrollContainer.scrollTop - pixelsPerStep
+        );
+        if (scrollContainer.scrollTop <= targetY) {
+          return;
+        }
+      } else {
+        scrollContainer.scrollTop = Math.min(
+          targetY,
+          scrollContainer.scrollTop + pixelsPerStep
+        );
+
+        if (scrollContainer.scrollTop >= targetY) {
+          return;
+        }
+      }
+
+      window.requestAnimationFrame(stepFunc);
+    };
+
+    window.requestAnimationFrame(stepFunc);
+  }
 }
